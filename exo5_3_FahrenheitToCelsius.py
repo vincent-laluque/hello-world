bfb20d2c1c8e172239ab11e7f9517c204ed0f610
Vincent Laluque <vincent.laluque@free.fr>
2024-10-04 14:21:14 +0200

    FahrenheitCelsiusImplementation

---
+++ exo5_3_FahrenheitToCelsius.py
@@ -0,0 +1,10 @@
+# Transformer une température en degrés Fahrenheit en température en degrés Celsius
+
+fahrenheits = 25
+
+celsius =  (fahrenheits-32)/1.8
+print(fahrenheits," ° F = ",celsius," ° C")
+
+celsius = 25
+fahrenheits = (celsius*1.8) + 32
+print(celsius," ° C = ",fahrenheits," ° F")
