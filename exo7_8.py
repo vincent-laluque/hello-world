b7c74813f6efe72cc2a16f0c7b055de1a79da755
Vincent Laluque <vincent.laluque@free.fr>
2025-03-07 14:41:05 +0100

    pabon

---
+++ exo7_8.py
@@ -0,0 +1,16 @@
+
+from dessins_tortue import *
+
+up() # relever le crayon
+goto(-300,0)
+
+down()
+triangle(100,"red",0)
+up()
+right(90)
+forward(43)
+down()
+
+triangle(100,"green",135)
+
+a = input()
