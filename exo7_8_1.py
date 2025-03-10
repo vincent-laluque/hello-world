a48eb2d02922b18dade1133f66fe6c07ee56f6b1
Vincent Laluque <vincent.laluque@free.fr>
2025-03-10 16:39:19 +0100

    galaxie2

--- exo7_8_1.py
+++ exo7_8_1.py
@@ -9,8 +9,14 @@ move = 80
 right(120)
 k = 0
 while (k < 5):
+    if (k < 3):
+        pas = (k % 3) + 1
+    else:
+        pas = 2 - (k % 3)
+    # write(pas)
+    move = 40 + (pas*20)
     k += 1
-    etoile6(move,"blue",0)
+    etoile6(move,"red",0)
     forward(move+10)
     down()
     carre(move,"blue")
